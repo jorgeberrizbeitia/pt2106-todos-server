434aaf3d291b10dd8e2a383eddc1ea22d0e3b70c
Alntjan <oliveiracatarino88@gmail.com>
2021-10-27 09:23:25 +0100

    M3W5D1

--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -1,16 +1,16 @@
-const router = require("express").Router();
-const Todo = require("../models/Todo.model");
+const router = require('express').Router();
+const Todo = require('../models/Todo.model');
 
 // Create a new ToDo element.
-router.post("/create", (req, res, next) => {
-  const { title, description, doBefore } = req.body;
-  Todo.create({ title, description, doBefore })
+router.post('/create', (req, res, next) => {
+  const { title, description, doBefore, imageUrl } = req.body;
+  Todo.create({ title, description, doBefore, imageUrl })
     .then((data) => res.json(data))
     .catch((err) => next(err));
 });
 
 // Display the list of All ToDo (only titles).
-router.get("/all", (req, res, next) => {
+router.get('/all', (req, res, next) => {
   // Todo.find()
   Todo.find({}, { title: 1 })
     .then((data) => res.json(data))
@@ -18,22 +18,22 @@ router.get("/all", (req, res, next) => {
 });
 
 // Display a specific ToDo element with all info.
-router.get("/:id", (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   Todo.findById(req.params.id)
     .then((data) => res.json(data))
     .catch((err) => next(err));
 });
 
 // Delete a specific ToDo element.
-router.delete("/:id", (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   Todo.findByIdAndDelete(req.params.id)
-    .then((data) => res.json("All good!" + data._id))
+    .then((data) => res.json('All good!' + data._id))
     .catch((err) => next(err));
 });
 
 // Edit a specific ToDo element.
 // we can also do a PUT route instead of PATCH
-router.patch("/:id", (req, res, next) => {
+router.patch('/:id', (req, res, next) => {
   const { title, description, doBefore } = req.body;
   Todo.findByIdAndUpdate(
     req.params.id,
